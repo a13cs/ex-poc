c9da57414dca14c2c8c69f7084de563c442ea41e
alex <alenhome1@gmail.com>
2021-06-25 13:23:22 -0400

    Added timestamp chart.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
-import { createChart, CrosshairMode } from 'lightweight-charts';
+import {createChart, CrosshairMode, UTCTimestamp} from 'lightweight-charts';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -9,10 +10,17 @@ import { createChart, CrosshairMode } from 'lightweight-charts';
 export class AppComponent implements OnInit {
   title = 'app';
 
+  constructor(private http: HttpClient) {
+  }
+
   ngOnInit(): void {
     const chart = createChart(document.body, {
       width: 500,
       height: 400,
+      timeScale: {
+        timeVisible: true,
+        secondsVisible: false,
+      },
       crosshair: {
         mode: CrosshairMode.Normal,
       }
@@ -25,14 +33,15 @@ export class AppComponent implements OnInit {
     points[0]["high"] = 2;
     points[0]["low"] = 1;
     points[0]["close"] = 2;
-    points[0]["time"] = '2019-04-11'
+    // points[0]["time"] = '2019-04-11'
+    points[0]["time"] = 1622211820
 
     points[1] = [];
     points[1]["open"] = 2;
     points[1]["high"] = 3;
     points[1]["low"] = 2;
     points[1]["close"] = 3;
-    points[1]["time"] = '2019-04-12'
+    points[1]["time"] = 1622221820
 
     series.setData(points);
     // const lineSeries = chart.addLineSeries();
@@ -49,12 +58,56 @@ export class AppComponent implements OnInit {
     //   { time: '2019-04-20', value: 74.43 },
     // ]);
 
+    const chartLine = createChart(document.body, {
+      width: 600,
+      height: 300,
+      layout: {
+        backgroundColor: '#ffffff',
+        textColor: 'rgba(33, 56, 77, 1)',
+      },
+      grid: {
+        vertLines: {
+          color: 'rgba(197, 203, 206, 0.7)',
+        },
+        horzLines: {
+          color: 'rgba(197, 203, 206, 0.7)',
+        },
+      },
+      timeScale: {
+        timeVisible: true,
+        secondsVisible: true,
+        // fixLeftEdge: true
+      },
+    });
 
-    let smaLine = chart.addLineSeries({
+    let smaLine = chartLine.addLineSeries({
       color: 'rgba(4, 111, 232, 1)',
       lineWidth: 2,
     });
-    smaLine.setData([{time: points[0]["time"], value: 2},{time: points[1]["time"], value: 3}]);
+
+    // let data: any[] = [
+    //   {time: 1622211820, value: 1.0},
+    //   {time: 1622222825, value: 2.5},
+    //   {time: 1622233830, value: 3.0},
+    //   {time: 1622244835, value: 4.5},
+    //   {time: 1622255840, value: 5.0},
+    //   {time: 1622266845, value: 6.5},
+    //   {time: 1622277850, value: 7.0},
+    //   {time: 1622288855, value: 8.5},
+    // ];
+    // smaLine.setData(data);
+
+    // this.http.get<any[]>('assets/ema_time_close.json').subscribe(
+    this.http.get<any[]>('/be/indicator/a/0').subscribe(
+      d => {
+        console.log(d)
+        let data: any[] = []
+        d.forEach(point => {
+          data.push({time: +point[0] as UTCTimestamp, value: +point[1]})
+        } )
+        smaLine.setData(data)
+      }
+    );
 
   }
 
